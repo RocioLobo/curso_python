007aab9971222de63a87c70396ab3cee73a7515f
RocioRosalindaLoboJacinto <lobojacintorociorosalinda#@gmail.com>
2024-05-13 10:23:01 -0500

    cree mi archivo curso pyton

--- controles de_flujo/README.md
+++ controles de_flujo/README.md
@@ -6,10 +6,40 @@ existen maneras de romper con el flujo normal de los programas creando `bifurcac
 la sentencia de decicion en pyton es `if` .en su estructura debemos añadir una **exprecion de comparacion** terminando con `:` al final de la linea
 > ejemplo:
 
-```pyton
+```python
 if true:
     print("es verdad")
 ```
+>else - elif
+```pyton
+edad:int=(input("escribe tu edad"))
+if edad>=18:
+print("eres mayor")
+else:
+    print("eres menor de edad")
+```
+## segundo ejemplo if almacena do en variables:
+```python
+
+
 ## ciclos
 son los controles de flujo que repiten codigo
-y sintaxis
+y sintaxis en la siguiente
+### for
+
+> para for:
+``` pyton
+# este codigo imprime los numeros
+# del 1 al 10
+for n in range (1;11):
+    print(n)
+    
+## ejemplo
+## for n in  "aeiou" te permite mostrar las letras
+    print(n)
+## for i,l inenumerate("aeiou") permite mostrar el indice y la letra
+       print(f" el indice es{i} y la letra es {l}")
+
+```
+> para while
+
--- controles de_flujo/lecture.py
+++ controles de_flujo/lecture.py
@@ -1,8 +1,58 @@
 # ejemplo de if
 if true:
-    print
-
+    print("verdad")
 
 #ejemplo de for
 #imprimir los numeros pares
-#entre
+#entre 1 -20
+for n in range(1,21):
+    if n%2==0:
+        print(n)
+
+#crear un programa que me imprima las 5vocales
+vocales:str="aeiou"
+for n in range (0,5):
+    print(vocales[n])
+
+# crear un programa que me muestre los 8 primeros numeros pares.
+contador=0
+for n in range (1,17):
+    if n%2==0:
+        contador+=1
+        print(f"{n} es el numero{contador} ")
+
+# crear un programa que pida al usuario escribir una oracion
+# y mostrar por terminal la cantidad de vocales a que
+# tiene esa oracion
+# OJO:SOLO LAS "a" MINUSCULA
+
+oracion:str=input("escribe una oracion: ")
+contador:int=0
+for n in range(0,len(oracion)):
+    if oracion[n]=="a":
+        contador=contador+1
+        #contador+=1
+print(f"la cantidad de letras a que  tengo e es {contador}")
+
+# cear un programa que me comente la cantaidad de comas y me muestre
+#sus indices.
+#OJO. tiene que pedir al usuario
+cantidad:str=input()
+
+
+
+
+
+
+
+
+# ejemplos de if - else primer ejemplo de if estructurado:
+
+edad:int=int(input("escribe tu edad"))
+if edad>18:
+    print("eres mayor")
+else:
+    print("eres menor de edad")
+
+
+
