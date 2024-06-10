adb8b3dc5ca2c0e1e7627455af689a43b6854180
RocioRosalindaLoboJacinto <lobojacintorociorosalinda#@gmail.com>
2024-06-10 10:25:59 -0500

    clase de funciones terminada

--- funciones/lecture.py
+++ funciones/lecture.py
@@ -1,5 +1,6 @@
 # return devuelve valores que podre hacer uso
 # crear una funcion que retorne el numero 10 y muestra por terminal si es par
+# siempre que el valor que retorne mi funcion se utilize en mas sentencias u operaciones hacer uso de return
 def diez():
     return 10
 if diez()%2==0:
